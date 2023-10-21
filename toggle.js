edb6a382d7bd1179aa9bc5d82a71bb3bbac45922
tomimrc <tomasgarciacaffieri@gmail.com>
2023-10-21 12:49:17 -0300

    completo toggle

---
+++ toggle.js
@@ -0,0 +1,10 @@
+
+
+
+const btn = document.getElementById("btn")
+const container = document.getElementById("container")
+
+btn.addEventListener("click", () => {
+
+    btn.classList.toggle("toggle")
+})
